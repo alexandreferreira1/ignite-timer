f84d194737c67a701457883164b52e66d079e9c5
alexandreximenez <alexandre.x.f@hotmail.com>
2024-10-13 20:35:45 -0300

    header and home component

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
     return (
-        <div>
+        <LayoutContainer>
             <Header />
             {/* O conteúdo que vai variar vem nesse Outlet. O Header aparece em todas as páginas */}
             <Outlet />
-        </div>
+        </LayoutContainer>
     )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,11 +16,13 @@ export const GlobalStyle = createGlobalStyle`
         background: ${(props) => props.theme["gray-900"]};
         color: ${(props) => props.theme["gray-300"]};
 
-        body, input, textarea, button {
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
             font-family: 'Roboto', sans-serif;
             font-weight: 400;
             font-size: 1rem;
-        }
     }
-
 `;
+
